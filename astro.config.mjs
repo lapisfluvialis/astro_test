d8b0328c3c05ddc4a6df5850638707078400e462
lapisfluvialis <39161166+lapisfluvialis@users.noreply.github.com>
2023-03-03 13:10:37 +0900

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://lapisfluvialis.github.io',
+    base: '/astro_test'
+});
